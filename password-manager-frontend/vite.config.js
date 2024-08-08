f34f1425d2a100441c25c47cac544e22bf853f8f
Duartini <Emanuel-d@hotmail.com>
2024-08-07 22:58:39 -0600

    password-manager

---
+++ password-manager-frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    hmr: {
+      protocol: 'ws', // o 'wss' si estás usando HTTPS
+      host: '172.28.0.2', // o la IP/hostname de tu servidor
+    },
+  },
+});
