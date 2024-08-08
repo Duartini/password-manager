6b0890e474cf44388239d403a0c2cac4b2588659
Duartini <Emanuel-d@hotmail.com>
2024-08-07 23:25:00 -0600

    update-package

--- password-manager-frontend/vite.config.js
+++ password-manager-frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     host: true,
     hmr: {
       protocol: 'ws', // o 'wss' si estás usando HTTPS
