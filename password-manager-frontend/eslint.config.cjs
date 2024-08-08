fb82938140c7f97ad149f3004228dbd66458ac74
Duartini <Emanuel-d@hotmail.com>
2024-08-08 01:35:25 -0600

    cjs-5

--- password-manager-frontend/eslint.config.cjs
+++ password-manager-frontend/eslint.config.cjs
@@ -15,22 +15,17 @@ const eslintConfig = {
   },
   plugins: {
     react: require('eslint-plugin-react'),
-    '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
     // Agrega más plugins si es necesario
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
     // Agrega más configuraciones extendidas si es necesario
   ],
   rules: {
     'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off', // Para Next.js o si usas un entorno que no requiere React en el alcance
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/ban-types': 'warn',
     // Agrega más reglas según las necesidades de tu proyecto
   },
   settings: {
