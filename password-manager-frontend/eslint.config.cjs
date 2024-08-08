eb395178fafa46238e2268e4da84a5760025522c
Duartini <Emanuel-d@hotmail.com>
2024-08-08 01:39:03 -0600

    cjs-6

--- password-manager-frontend/eslint.config.cjs
+++ password-manager-frontend/eslint.config.cjs
@@ -1,38 +1,43 @@
-/** @type {import('eslint').Linter.Config} */
-const eslintConfig = {
-  languageOptions: {
-    globals: {
-      browser: true,
-      es2021: true,
+/** @type {import('eslint').Linter.FlatConfig} */
+const eslintConfig = [
+  // Configuración de los plugins
+  {
+    plugins: {
+      react: require('eslint-plugin-react'),
     },
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
+  },
+  // Configuración de las opciones del lenguaje
+  {
+    languageOptions: {
+      globals: {
+        browser: true,
+        es2021: true,
+      },
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
   },
-  plugins: {
-    react: require('eslint-plugin-react'),
-    // Agrega más plugins si es necesario
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    // Agrega más configuraciones extendidas si es necesario
-  ],
-  rules: {
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off', // Para Next.js o si usas un entorno que no requiere React en el alcance
-    // Agrega más reglas según las necesidades de tu proyecto
-  },
-  settings: {
-    react: {
-      version: 'detect', // Detecta automáticamente la versión de React
+  // Configuración de las reglas
+  {
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off', // Para Next.js o si usas un entorno que no requiere React en el alcance
     },
   },
-};
+  // Configuración de plugins de configuración adicional si es necesario
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
 
 module.exports = eslintConfig;
